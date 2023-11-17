e0cb328da4540a55ae3dafb64b468c390286cff1
cdasilvasantos <cd58@njit.edu>
2023-11-17 01:52:26 -0500

    made the description box smaller

--- todo/forms.py
+++ todo/forms.py
@@ -6,3 +6,6 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoItem
         fields = ['title', 'description']
+
+    # Customize the description field
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 1}))  # Adjust the 'rows' value as needed
